20c56385ce9182c4c7fbd9427685594030213f1e
y0k41 <jonathannogiri@gmail.com>
2022-01-08 15:04:48 +0000

    Format date/time correction

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ def date(message):
     pst = pytz.timezone('America/Los_Angeles')
     ist = pytz.timezone('Asia/Calcutta')
     pt = pytz.timezone('Europe/London')
-    bot.send_message(message.chat.id, f'Current Date Time in UTC = {datetime.now(tz=utc)}')
-    bot.send_message(message.chat.id, f'Current Date Time in PST = {datetime.now(pst)}')
-    bot.send_message(message.chat.id, f'Current Date Time in IST = {datetime.now(ist)}')
-    bot.send_message(message.chat.id, f'Current Date Time in BST = {datetime.now(pt)}',)
+    utc_string = datetime.now(tz=utc)
+    pst_string = datetime.now(pst)
+    ist_string = datetime.now(ist)
+    pt_string = datetime.now(pt)
+    bot.send_message(message.chat.id, f'Current Date Time in UTC = {utc_string.strftime("%A, %d. %B %Y %I:%M%p")}')
+    bot.send_message(message.chat.id, f'Current Date Time in PST = {pst_string.strftime("%A, %d. %B %Y %I:%M%p")}')
+    bot.send_message(message.chat.id, f'Current Date Time in IST = {ist_string.strftime("%A, %d. %B %Y %I:%M%p")}')
+    bot.send_message(message.chat.id, f'Current Date Time in BST = {pt_string.strftime("%A, %d. %B %Y %I:%M%p")}',)
 
 
 bot.polling()
